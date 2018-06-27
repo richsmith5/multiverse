d9a7e685de09332ecedb691538a38f70241193e5
richsmith5 <40637615+richsmith5@users.noreply.github.com>
2018-06-27 10:25:09 -0500

    Add files via upload

---
+++ workspace/bull.js
@@ -0,0 +1,23 @@
+function consolePrint1(){
+    console.log("hello Jon");
+  }
+   function alertPrint2(){
+    alert("hello Jon");
+  }
+   function documentPrint3(){
+    document.write("hello jon")
+  }
+  function consolePrint(){
+    console.log("Whats good Rich");
+  }
+   function alertPrint(){
+    alert("Whats good Rich");
+  }
+   function documentPrint(){
+    document.write("whats good Rich")
+  }
+  function chBackcolor(color) {
+    document.body.style.background = color;
+ }
+ 
+ 
---
+++ workspace/main.js
@@ -0,0 +1,2 @@
+Function mymethod (){p2 style="color:red"}
+Funtion mymethod2 () {}
